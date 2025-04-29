c4f4f1044f1ae4a146f259eaee92b1c72d8635de
SeppyDev <53712490+Seppy-Dev@users.noreply.github.com>
2025-04-29 22:00:31 +0100

    Add files via upload

---
+++ lavachicken.sh
@@ -0,0 +1,292 @@
+clear
+
+# La-la-la-Lava
+echo "La-" | toilet
+sleep 0.15
+echo "La-" | toilet
+sleep 0.15
+echo "La-" | toilet
+sleep 0.15
+echo "La" | toilet
+sleep 0.15
+clear
+
+echo "La-" | toilet
+echo "La-" | toilet
+echo "La-" | toilet
+echo "Lava" | toilet
+sleep 0.5
+clear  
+
+
+
+# ch-ch-ch-chicken
+echo "Ch-" | toilet
+sleep 0.15
+echo "Ch-" | toilet
+sleep 0.15
+echo "Ch-" | toilet
+sleep 0.15
+echo "Chi" | toilet
+sleep 0.15
+clear
+
+echo "Ch-" | toilet
+echo "Ch-" | toilet
+echo "Ch-" | toilet
+echo "Chicken" | toilet
+sleep 0.5
+clear  
+
+
+
+# Steve's lava chicken
+echo "Steve's" | toilet
+sleep 0.5
+echo "La" | toilet
+sleep 0.2
+clear
+
+echo "Steve's" | toilet
+echo "Lava" | toilet
+sleep 0.2
+echo "Chi" | toilet
+sleep 0.25
+clear
+
+echo "Steve's" | toilet
+echo "Lava" | toilet
+echo "Chicken" | toilet
+sleep 0.25
+clear
+
+
+
+# Yeah, it's tasty as hell
+echo "yeah," | toilet
+sleep 0.15
+echo "it's" | toilet
+sleep 0.15
+echo "tas" | toilet
+sleep 0.15
+clear
+
+echo "yeah," | toilet
+echo "it's" | toilet
+echo "tasty" | toilet
+sleep 0.15
+echo "as" | toilet
+sleep 0.15
+echo "hell!" | toilet
+sleep 1
+clear
+
+
+
+# Ooh, mamacita
+echo "Ooh" | toilet
+sleep 0.3
+echo "ma," | toilet
+sleep 0.15
+clear
+
+echo "Ooh" | toilet
+echo "mama," | toilet
+sleep 0.15
+clear
+
+echo "Ooh" | toilet
+echo "mamaci," | toilet
+sleep 0.15
+clear
+
+echo "Ooh" | toilet
+echo "mamacita," | toilet
+sleep 0.15
+clear
+
+
+
+# Now you're ringin the bell
+echo "now" | toilet
+sleep 0.2
+echo "you're" | toilet
+sleep 0.2
+echo "ring'" | toilet
+sleep 0.15
+clear
+
+echo "now" | toilet
+echo "you're" | toilet
+echo "ringing'" | toilet
+sleep 0.2
+echo "the" | toilet
+sleep 0.2
+echo "bell!" | toilet
+sleep 0.5
+clear
+
+
+
+# Crispy and juicy
+echo "cris" | toilet
+sleep 0.3
+clear
+
+echo "crispy" | toilet
+sleep 0.1
+echo "and" | toilet
+sleep 0.15
+echo "jui" | toilet
+sleep 0.15
+clear
+
+echo "crispy" | toilet
+echo "and" | toilet
+echo "juicy" | toilet
+sleep 0.2
+clear
+
+
+
+# Now you're havin a snack
+echo "now" | toilet
+sleep 0.15
+echo "you're" | toilet
+sleep 0.15
+echo "ha" | toilet
+sleep 0.15
+clear
+
+echo "now" | toilet
+echo "you're" | toilet
+echo "havin'" | toilet
+sleep 0.15
+echo "a" | toilet
+sleep 0.1
+echo "snack" | toilet
+sleep 0.5
+clear
+
+
+
+# Ooh, super spicy
+echo "Ooh" | toilet
+sleep 0.2
+echo "su" | toilet
+sleep 0.1
+clear
+
+echo "Ooh" | toilet
+echo "super" | toilet
+sleep 0.1
+echo "spi" | toilet
+sleep 0.1
+clear
+
+echo "Ooh" | toilet
+echo "super" | toilet
+echo "spicy" | toilet
+sleep 0.2
+clear
+
+# It's a lava attaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaack
+echo "It's" | toilet
+sleep 0.15
+echo "a" | toilet
+sleep 0.15
+echo "la" | toilet
+sleep 0.15
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+sleep 0.2
+echo "att" | toilet
+sleep 0.1
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "atta" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaaaaaa" | toilet
+sleep 0.05
+clear
+
+echo "It's" | toilet
+echo "a" | toilet
+echo "lava" | toilet
+echo "attaaaaaaaaaaaaa" | toilet
+sleep 0.05
+clear
